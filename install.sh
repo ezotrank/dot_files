1bb23cda0395c9db20b209e9bc23dc7a720345f0
Maxim Kremenev <ezotrank@gmail.com>
2012-04-23 21:26:57 +0300

    create install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+REPO_DIR=`pwd`
+
+echo "Submodule init and update"
+git submodule init
+git submodule update
+
+echo "Start create links"
+ln -snf $REPO_DIR/.zshrc ~/.zshrc
+ln -snf $REPO_DIR/.zsh ~/.zsh
+ln -snf $REPO_DIR/.tmux ~/.tmux
+ln -snf $REPO_DIR/.screenrc ~/.screenrc
+ln -snf $REPO_DIR/.newsbeuter ~/.newsbeuter
+
+if ! [[ -d ~/.config ]]; then mkdir ~/.config; fi
+ln -snf $REPO_DIR/.config/awesome ~/.config/awesome
